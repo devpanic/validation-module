f69918f29525c903e426c3a73ddcf57022509d18
devpanic <yeon.dev.8@gmail.com>
2024-11-20 21:52:32 +0900

    Initial Commit

---
+++ src/main/java/com/devpanic/validationmodule/ValidationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.devpanic.validationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devpanic/validationmodule/ValidationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpanic.validationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
